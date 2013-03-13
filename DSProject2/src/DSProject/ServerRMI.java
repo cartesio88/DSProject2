5a4f23d21bc7979a5ea9a3965476d2b58babffa9
Cesar MC <cesarmoracastro@gmail.com>
2013-03-13 10:48:08 -0500

    Now jar works. Implemented the changing role server/coordinator functionality :)

--- DSProject2/src/DSProject/ServerRMI.java
+++ DSProject2/src/DSProject/ServerRMI.java
@@ -1,49 +1,74 @@
 package DSProject;
 
 import java.net.InetAddress;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class ServerRMI extends UnicastRemoteObject implements ServerInterface{
+public class ServerRMI extends UnicastRemoteObject implements ServerInterface {
 
 	private static final long serialVersionUID = 1L;
 	private int serverPort = 0;
 	private InetAddress serverIp = null;
+	private int coordinatorPort = 0;
+	private InetAddress coordinatorIp = null;
 	private boolean isCoordinator = false;
 	private String serverName = "";
+	private String coordinatorName = "";
 	private ServerInterface coordinator = null;
 
-	//private LinkedList<Article> articles;
+	// private LinkedList<Article> articles;
 	private BulletinBoard bulletinBoard;
 	private int nextArticleId = 0;
 
-	public ServerRMI(InetAddress serverIp, int serverPort, boolean isCoordinator)
-			throws RemoteException {
+	public ServerRMI(InetAddress serverIp, int serverPort,
+			boolean isCoordinator, InetAddress coordinatorIp,
+			int coordinatorPort) throws RemoteException, NotBoundException {
 		super();
 
 		this.serverIp = serverIp;
 		this.serverPort = serverPort;
 		this.isCoordinator = isCoordinator;
-		
-		serverName = serverIp+":"+serverPort;
-		
+		this.coordinatorIp = coordinatorIp;
+		this.coordinatorPort = coordinatorPort;
+		serverName = serverIp + ":" + serverPort;
+		if(!isCoordinator) coordinatorName = serverIp + ":" + coordinatorPort;
+
+		// Printing info
+		System.out.println(" == Server info == ");
+		if (isCoordinator)
+			System.out.println("Role: COORDINATOR");
+		else
+			System.out.println("Role: REGULAR SERVER");
+		System.out.println("Address: " + serverIp + ":" + serverPort);
+		System.out.println("Binding Name: \"" + serverName+ "\"");
+		if (!isCoordinator) {
+			System.out.println("Coordinator Address: " + coordinatorIp + ":"
+					+ coordinatorPort);
+			System.out.println("Coordinator Binding Name: \"" + coordinatorName + "\"");
+		}
+
 		System.setProperty("java.rmi.server.hostname",
 				serverIp.getHostAddress());
-
 		System.out.println("Starting the Server: "
 				+ System.getProperty("java.rmi.server.hostname"));
 		System.setProperty("java.net.preferIPv4Stack", "true");
 
 		bulletinBoard = new BulletinBoard();
 
-		Registry registry = LocateRegistry.createRegistry(serverPort);
-		registry.rebind(serverName, this);
-		
-		if(!isCoordinator){ // Binding with the coordinator
-			// TODO
+		// Creating local registry
+		Registry localRegistry = LocateRegistry.createRegistry(serverPort);
+		localRegistry.rebind(serverName, this);
+
+		if (!isCoordinator) { // Binding with the coordinator
+			Registry coordinatorRegistry = LocateRegistry.getRegistry(
+					coordinatorIp.getHostAddress(), coordinatorPort);
+
+			coordinator = (ServerInterface) coordinatorRegistry.lookup(coordinatorName);
 		}
+
 	}
 
 	@Override
@@ -52,7 +77,7 @@ public class ServerRMI extends UnicastRemoteObject implements ServerInterface{
 		int articleId = coordinator.getNextId();
 		Article article = new Article(articleId, title, content);
 		bulletinBoard.addArticle(article);
-		
+
 		return false;
 	}
 
@@ -61,12 +86,13 @@ public class ServerRMI extends UnicastRemoteObject implements ServerInterface{
 		System.out.println("Reading the articles!");
 		return bulletinBoard.ReadArticlesList();
 	}
-	
+
 	@Override
 	public String Choose(int id) throws RemoteException {
 		Article a = bulletinBoard.getArticle(id);
-		if(a == null){
-			System.out.println("The article with ID: "+id+" does no exist in this server!");
+		if (a == null) {
+			System.out.println("The article with ID: " + id
+					+ " does no exist in this server!");
 			return "";
 		}
 		return a.completeArticle();
@@ -76,18 +102,18 @@ public class ServerRMI extends UnicastRemoteObject implements ServerInterface{
 	public boolean Reply(int id, String content) throws RemoteException {
 		int nextId = coordinator.getNextId();
 		return bulletinBoard.reply(id, nextId, content);
-		
+
 	}
 
 	@Override
 	public int getNextId() throws RemoteException {
-		if(!isCoordinator){
-			System.out.println("Asking for the next Id to a server that is not the coordinator!");
-		}else{
+		if (!isCoordinator) {
+			System.out
+					.println("Asking for the next Id to a server that is not the coordinator!");
+		} else {
 			nextArticleId++;
-		}			
-		return nextArticleId-1;
+		}
+		return nextArticleId - 1;
 	}
-
 
 }
--- DSProject2/src/Server.java
+++ DSProject2/src/Server.java
@@ -1,27 +1,67 @@
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.Enumeration;
 import java.util.Scanner;
 
 import DSProject.ServerRMI;
 
-
 public class Server {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 		System.setProperty("java.net.preferIPv4Stack", "true");
 		System.setProperty("java.rmi.server.codebase", "file:./bin");
 		System.setProperty("java.security.policy", "file:./policyfile");
-		Scanner scan = new Scanner(System.in); 
-		int serverRMIPort = portCheck(args[0],scan);
-		
+
+		if (args.length != 4 && args.length != 2) {
+			System.out
+					.println("Usage: java -jar Server.jar server [port] [coordinator ip] [coordinator port]# For regular server");
+			System.out
+					.println("       java -jar Server.jar coordinator [port] # For coordinator");
+			return;
+		}
+		Scanner scan = new Scanner(System.in);
+
+		int coordinatorPort = 0;
+		InetAddress coordinatorIp = null;
+		int serverPort = portCheck(args[1], scan);
 		InetAddress serverIp = getServerIP();
-				
 		boolean isCoordinator = false;
-		ServerRMI server = new ServerRMI(serverIp, serverRMIPort, isCoordinator);	
+		if (args[0].equalsIgnoreCase("server")) {
+			System.out.println("Configuring as a regular server.");
+			coordinatorPort = portCheck(args[3], scan);
+			try {
+				coordinatorIp = InetAddress.getByName(args[2]);
+			} catch (UnknownHostException e) {
+				System.out.println("Unknown coordinator ip: "+coordinatorIp);
+				e.printStackTrace();
+				return;
+			}
+			isCoordinator = false;
+		} else if (args[0].equalsIgnoreCase("server")) {
+			System.out.println("Configurins as the coordinator.");
+			isCoordinator = true;
+		} else {
+			System.out.println("[ERROR] Invalid role: " + args[0]);
+			return;
+		}
+
+		try {
+			@SuppressWarnings("unused")
+			ServerRMI server = new ServerRMI(serverIp, serverPort, isCoordinator,
+					coordinatorIp, coordinatorPort);
+		} catch (RemoteException e) {
+			System.out.println("Error creating the RMI object server");
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			System.out.println("Error trying to bind with the coordinator");
+			e.printStackTrace();
+		}
 	}
-	
+
 	protected static InetAddress getServerIP() {
 		InetAddress serverIp = null;
 		try {
@@ -35,44 +75,45 @@ public class Server {
 					break;
 				}
 			}
-			System.setProperty("java.rmi.server.hostname",serverIp.getHostAddress());
-			
+			System.setProperty("java.rmi.server.hostname",
+					serverIp.getHostAddress());
+
 		} catch (SocketException e) {
 			System.out.println("ERROR getting the interfaces of the device");
 			e.printStackTrace();
 		}
 		return serverIp;
 	}
-	
-	private static int portCheck(String s, Scanner scan){
+
+	private static int portCheck(String s, Scanner scan) {
 
 		int p = 0;
 		boolean badRange = true;
 		boolean notInt = true;
-				
-		if (s.length() == 0) p=1099;
-			else {
-				while (badRange || notInt) {
-			
-					try{
-						Integer.valueOf(s);
-						notInt = false;
-						p = Integer.valueOf(s);
-						}
-					catch(NumberFormatException e){
-						notInt = true;
-						System.out.println("Port has wrong format try again: ");
-						s = scan.nextLine();
-					}
-				
-					badRange = p < 0 || p > 55901;
-					if (badRange){
-						System.out.println("Port is out of range try again: ");
-						p = Integer.valueOf(scan.nextLine());
-					}
-			
+
+		if (s.length() == 0)
+			p = 1099;
+		else {
+			while (badRange || notInt) {
+
+				try {
+					Integer.valueOf(s);
+					notInt = false;
+					p = Integer.valueOf(s);
+				} catch (NumberFormatException e) {
+					notInt = true;
+					System.out.println("Port has wrong format try again: ");
+					s = scan.nextLine();
 				}
+
+				badRange = p < 0 || p > 55901;
+				if (badRange) {
+					System.out.println("Port is out of range try again: ");
+					p = Integer.valueOf(scan.nextLine());
+				}
+
 			}
+		}
 		return p;
 	}
 
